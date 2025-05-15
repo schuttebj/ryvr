701e3ddd82634d72123b173b160c94e5d9dbe194
Ben Schutte <schutte.bj@gmail.com>
2025-05-16 00:24:47 +0200

    Implement required abstract methods in DataForSEO_Service adapter

--- includes/api/class-dataforseo-service-adapter.php
+++ includes/api/class-dataforseo-service-adapter.php
@@ -84,4 +84,116 @@ class DataForSEO_Service extends API_Service {
         trigger_error('Call to undefined method ' . __CLASS__ . '::' . $name . '()', E_USER_ERROR);
         return null;
     }
+    
+    /**
+     * Calculate credits used for an API request.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $params   Request parameters.
+     * @param array  $response Response data.
+     * @return float Credits used.
+     */
+    protected function calculate_credits_used($endpoint, $params, $response) {
+        // Default credit cost
+        $credits = 1;
+        
+        // If the response contains a cost field, use that
+        if (isset($response['cost'])) {
+            $credits = (float)$response['cost'];
+        }
+        
+        return $credits;
+    }
+    
+    /**
+     * Execute the actual API request.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $params   Request parameters.
+     * @param string $method   HTTP method.
+     * @return array Response data.
+     */
+    protected function execute_request($endpoint, $params, $method) {
+        // Forward the request to the actual service
+        $url = $this->api_base_url . '/' . ltrim($endpoint, '/');
+        
+        $args = [
+            'method' => $method,
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode($this->api_key . ':' . $this->api_secret),
+                'Content-Type' => 'application/json',
+            ],
+            'timeout' => 30,
+        ];
+        
+        // Handle different HTTP methods
+        if ($method === 'GET') {
+            $url = add_query_arg($params, $url);
+        } else {
+            $args['body'] = wp_json_encode($params);
+        }
+        
+        // Forward to the service if it has a suitable method
+        if (method_exists($this->service, 'make_request')) {
+            return $this->service->make_request($endpoint, $params, $method);
+        }
+        
+        // Fall back to a standard WordPress HTTP request
+        $response = wp_remote_request($url, $args);
+        
+        if (is_wp_error($response)) {
+            throw new \Exception($response->get_error_message(), $response->get_error_code() ?: 500);
+        }
+        
+        $response_code = wp_remote_retrieve_response_code($response);
+        $response_body = wp_remote_retrieve_body($response);
+        
+        $data = json_decode($response_body, true);
+        
+        if ($response_code >= 400 || (isset($data['status_code']) && $data['status_code'] >= 400)) {
+            $error_message = isset($data['status_message']) ? $data['status_message'] : 'Unknown API error';
+            throw new \Exception($error_message, $response_code ?: 500);
+        }
+        
+        return $data;
+    }
+    
+    /**
+     * Generate a sandbox response for testing.
+     *
+     * @param string $endpoint API endpoint.
+     * @param array  $params   Request parameters.
+     * @param string $method   HTTP method.
+     * @return array Mock response data.
+     */
+    protected function generate_sandbox_response($endpoint, $params, $method) {
+        // Forward to the service if it has a suitable method
+        if (method_exists($this->service, 'test_connection')) {
+            return $this->service->test_connection();
+        }
+        
+        // Default sandbox response
+        return [
+            'status_code' => 20000,
+            'status_message' => 'Sandbox mode: OK',
+            'time' => date('Y-m-d\TH:i:s\Z'),
+            'cost' => 0,
+            'tasks_count' => 1,
+            'tasks_error' => 0,
+            'tasks' => [
+                [
+                    'id' => 'sandbox-' . uniqid(),
+                    'status_code' => 20000,
+                    'status_message' => 'Sandbox data',
+                    'time' => date('Y-m-d\TH:i:s\Z'),
+                    'result' => [
+                        'sandbox' => true,
+                        'endpoint' => $endpoint,
+                        'method' => $method,
+                    ],
+                ],
+            ],
+            'sandbox' => true,
+        ];
+    }
 } 
